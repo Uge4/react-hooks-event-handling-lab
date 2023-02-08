cc01b5e4c668028ad0315df0f871e4ba0a52c310
eugene <eugeneholenstein@gmail.com>
2023-02-09 06:31:08 +1100

    completed handling event labs

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
 // Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
+        
     return (
-        <div></div>
+        <input 
+            type="password" 
+            value="Enter Password"
+            onChange={e => console.log('Entering password...')}
+            
+        />
     )
 }
 
